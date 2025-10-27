02972b53f36173036abf66545dc48e1fdb797ee6
MattsDevLab <116913535+MattsDevLab@users.noreply.github.com>
2025-10-27 23:27:21 +0000

    Complete 001-hello-console

--- 01-beginner/001-hello-console/main.js
+++ 01-beginner/001-hello-console/main.js
@@ -1,1 +1,7 @@
-console.log("Hellow and welcome to the Monorepo 100 JS Challenge");
+let firstName = prompt("What is your name?");
+let introText = document.querySelector(".intro-text");
+
+if(introText){
+    introText.innerHTML = `Hello ${firstName} and welcome to the monorepo 100 JS Challenge.`;
+}
+
